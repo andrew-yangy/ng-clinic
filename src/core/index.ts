f60a6d2d680817ae24c054cec635c6b6fe046cc9
Andrew Yang <ddvkid@gmail.com>
2019-04-28 21:04:58 +1000

    inject detectChanges function and modifyNodeOpacity

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,53 @@
 declare const getAllAngularRootElements: () => Element[];
-const root = getAllAngularRootElements();
-console.log(root);
+declare const ng;
+declare const Zone;
+
+let [root] = getAllAngularRootElements();
+let appRoot = ng.probe(root);
+let [rootComponent] = appRoot.injector.get(ng.coreTokens.ApplicationRef).components;
+console.log(rootComponent);
+let ChangeDetectorRef = rootComponent.changeDetectorRef.constructor.prototype;
+ChangeDetectorRef.constructor.prototype.detectChanges = (function () {
+    let oldDC = ChangeDetectorRef.constructor.prototype.detectChanges;
+    let map = new WeakMap();
+
+    return function () {
+        Zone.root.run(() => showChangeDetection(this));
+        return oldDC.apply(this, arguments);
+    };
+
+    function showChangeDetection(changeDetector) {
+        let view = changeDetector._view;
+        modifyNodeOpacity(view, fade);
+        modifyNodeOpacity(view, (node) => {
+            let timeout = map.get(node.renderElement);
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+            timeout = setTimeout(() => show(node), 1000);
+            map.set(node.renderElement, timeout);
+        });
+    }
+
+    function modifyNodeOpacity(view, modifier) {
+        view.nodes.forEach(node => {
+            if (node && node.renderElement && node.renderElement.style) {
+                modifier(node);
+            }
+        });
+    }
+
+    function fade(node) {
+        let {style} = node.renderElement;
+        let opacity = parseFloat(style.opacity) || 1;
+        let newOpacity = opacity - 0.01;
+        style.display = 'block';
+        style.opacity = newOpacity > 0 ? newOpacity : 0;
+    }
+
+    function show(node) {
+        let {style} = node.renderElement;
+        style.display = 'block';
+        style.opacity = 1;
+    }
+})();
